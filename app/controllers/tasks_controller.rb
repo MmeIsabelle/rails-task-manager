af0ca86dedad5388bae4a21ac50dea1410d70bb0
Isabelle Lavoie <isabelle@kaastor.ca>
2021-08-10 12:11:47 -0700

    added the create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,8 +11,11 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
-
+  
   def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
   end
 
   def edit
@@ -25,6 +28,10 @@ class TasksController < ApplicationController
   end
 
   private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 
   def set_task
     @task = Task.find(params[:id])
